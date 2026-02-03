6ba5f2674ed4f898fc73e870f00f8971f81a82af
Sahil Khan <153730430+Sahil980427@users.noreply.github.com>
2026-02-03 20:51:34 +0530

    Update base path for portfolio deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sahil980427.github.io/sahilkhan-portfolio/",
+  base: "/sahilkhan-portfolio",
 })
